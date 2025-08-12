0de97b457e7da147efd5d669e5642a1cd78f1f21
Agrim Jaimini <aj638@cornell.edu>
2025-08-11 21:39:57 -0500

    fixed api

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,10 @@ import axios from 'axios';
 import { UserRegister, UserLogin, BountyCreate, BountyAccept, BountyClaim, PlatformStatistics } from '../types/api';
 
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000';
+const API_BASE_URL =
+  process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000';
+
+console.log('API_BASE_URL =', API_BASE_URL);
 
 const api = axios.create({
   baseURL: API_BASE_URL,
